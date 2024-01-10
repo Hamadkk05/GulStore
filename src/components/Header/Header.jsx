845e4300a4da067c9c6c29a8ec3467efe690aa0b
Hamadkk05 <140351542+Hamadkk05@users.noreply.github.com>
2024-01-11 00:55:25 +0500

    Adding SlideShowCart

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -11,46 +11,47 @@ import { Context } from "../../utils/context";
 
 import "./Header.scss";
 
-
 const Header = () => {
-    const [scrolled, setScrolled] = useState(false)
+  const [scrolled, setScrolled] = useState(false);
+  const [showCart, setShowCart] = useState(false);
 
-    const handleScroll = () => {
-        const offset = window.scrollY
-       if(offset > 200) {
-        setScrolled(true);
-
-       } else {
-        setScrolled(false);
-       }
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 200) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
     }
+  };
 
-    useEffect(() => {
-        window.addEventListener("scroll", handleScroll)
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
 
-    
-    }, [])
-    
-
-    return <header className={`main-header ${scrolled ? 'sticky-header': ''} `}>
+  return (
+    <>
+      <header className={`main-header ${scrolled ? "sticky-header" : ""} `}>
         <div className="header-content">
-            <ul className="left">
-                <li>Home</li>
-                <li>About</li>
-                <li>Categoty</li>
-            </ul>
-            <div className="center">GULSTORE.</div>
-            <div className="right">
-                <TbSearch/>
-                <AiOutlineHeart/>
-                <span className="cart-icon">
-                    <CgShoppingCart/>
-                    <span>5</span>
-                </span>
-            </div>
+          <ul className="left">
+            <li>Home</li>
+            <li>About</li>
+            <li>Categoty</li>
+          </ul>
+          <div className="center">GULSTORE.</div>
+          <div className="right">
+            <TbSearch />
+            <AiOutlineHeart />
+            <span className="cart-icon" onClick={()=> setShowCart(true)}>
+              <CgShoppingCart />
+              <span>5</span>
+            </span>
+          </div>
         </div>
-
-    </header>;
+      </header>
+      {showCart && <Cart setShowCart={setShowCart}/>}
+      
+    </>
+  );
 };
 
 export default Header;
